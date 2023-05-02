7de38b036551b8456a23cd236fa7b39d3af8adb9
adil-khursheed <adilkhursheed60@gmail.com>
2023-05-02 16:40:22 +0530

    app.js fixed

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ app.use(
     useTempFiles: true,
   })
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use("/api/v1", User);
 

7ab6f97eb78ba63888f2862715a735750d7426c3
adil-khursheed <adilkhursheed60@gmail.com>
2023-05-05 13:33:09 +0530

    token fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -129,6 +129,9 @@ export const logout = async (req, res) => {
       .status(200)
       .cookie("token", null, {
         expires: new Date(Date.now()),
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
       })
       .json({ success: true, message: "Logged out successfully" });
   } catch (error) {
--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,6 +6,9 @@ export const sendToken = (res, user, statusCode, message) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
   };
   const userData = {
     _id: user._id,

2d1895b90eb2ffb17129567a1c2355c02e7f3aaa
adil-khursheed <adilkhursheed60@gmail.com>
2023-05-06 13:01:03 +0530

    send token fixed

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,7 +6,6 @@ export const sendToken = (res, user, statusCode, message) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
     secure: true,
     sameSite: "none",
   };

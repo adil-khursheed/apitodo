c7fdafbe32746daf3c7b449e27e908ecdb6db25e
adil-khursheed <adilkhursheed60@gmail.com>
2023-05-14 11:18:21 +0530

    added app link

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://todo-beryl-pi.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })

1ab965a0ec7556cad20aaa809fb60fbafa599ecc
adil-khursheed <adilkhursheed60@gmail.com>
2023-05-20 15:09:42 +0530

    email service changed

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -2,8 +2,7 @@ import { createTransport } from "nodemailer";
 
 export const sendMail = async (to, subject, text) => {
   const transport = createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    service: process.env.SMTP_SERVICE,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
